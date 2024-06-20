65f3359afe68fe69ae283686dd63806688e201be
admin <a1exander.spb@yandex.ru>
2024-06-21 01:04:41 +0300

    1. Добавлен класс-конвертер вопроса в строку; 2. Убраны классы-принтеры; 3. Откорректирован класс TestServiceImpl в соответствии с правками; 4. Откорректирован файл spring-context.xml в соответствии с правками.

---
+++ hw01/src/main/java/ru/otus/hw/converters/QuestionToStringConverter.java
@@ -0,0 +1,20 @@
+package ru.otus.hw.converters;
+
+import lombok.RequiredArgsConstructor;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+
+@RequiredArgsConstructor
+public class QuestionToStringConverter {
+
+    public String convertQuestionToString(Question question) {
+        var stringBuilder = new StringBuilder();
+        var answers = question.answers().stream().map(Answer::text).toList();
+        var questionWithAnswers = stringBuilder.append(question.text()).append("\n");
+        for (int i = 0; i < answers.size(); i++) {
+            char letterOfAnswer = (char) (65 + i);
+            questionWithAnswers.append(letterOfAnswer).append(") ").append(answers.get(i)).append("\t\t\t");
+        }
+        return questionWithAnswers.append("\n").toString();
+    }
+}
