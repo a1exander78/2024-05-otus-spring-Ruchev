5e1d0bed20a0c80d8ba5cee9f3b18f24acb65b45
admin <a1exander.spb@yandex.ru>
2024-06-21 01:04:41 +0300

    1. Добавлен класс-конвертер вопроса в строку; 2. Убраны классы-принтеры; 3. Откорректирован класс TestServiceImpl в соответствии с правками; 4. Откорректирован файл spring-context.xml в соответствии с правками.

--- hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,46 +1,24 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.converters.QuestionToStringConverter;
 import ru.otus.hw.dao.QuestionDao;
-import ru.otus.hw.dao.dto.AnswerPrinter;
-import ru.otus.hw.dao.dto.QuestionPrinter;
 import ru.otus.hw.domain.Question;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
+    private final IOService ioService;
+
     private final QuestionDao questionDao;
 
-    private final QuestionPrinter questionPrinter;
-
-    private final AnswerPrinter answerPrinter;
+    private final QuestionToStringConverter questionToStringConverter;
 
     @Override
     public void executeTest() {
-        printQuestionsAndAnswers(questionDao.findAll());
-    }
-
-    private void printQuestionsAndAnswers(List<Question> questions) {
-
-        for (int i = 0; i < questions.size(); i++) {
-
-            int numberOfQuestion = i + 1;
-            var currentQuestion = questions.get(i);
-
-            questionPrinter.printQuestionText(numberOfQuestion, currentQuestion);
-
-            var answers = questions.get(i).answers();
-
-            for (int j = 0; j < answers.size(); j++) {
-
-                int numberOfAnswer = j + 1;
-                var currentAnswer = answers.get(j);
-
-                answerPrinter.printAnswerText(numberOfAnswer, currentAnswer);
-                answerPrinter.printAnswerCorrectness(currentAnswer);
-            }
+        var questions = questionDao.findAll();
+        for (Question currentQuestion : questions) {
+            ioService.printLine(questionToStringConverter.convertQuestionToString(currentQuestion));
         }
     }
 }
