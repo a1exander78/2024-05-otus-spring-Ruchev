4eee5d64ac26d438aea8e5416906e3cd0f060eba
admin <a1exander.spb@yandex.ru>
2024-06-21 01:44:32 +0300

    1. Убрана ненужная аннотация @RequiredArgsConstructor из класса-конвертера

--- hw01/src/main/java/ru/otus/hw/converters/QuestionToStringConverter.java
+++ hw01/src/main/java/ru/otus/hw/converters/QuestionToStringConverter.java
@@ -1,10 +1,8 @@
 package ru.otus.hw.converters;
 
-import lombok.RequiredArgsConstructor;
 import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Question;
 
-@RequiredArgsConstructor
 public class QuestionToStringConverter {
 
     public String convertQuestionToString(Question question) {
