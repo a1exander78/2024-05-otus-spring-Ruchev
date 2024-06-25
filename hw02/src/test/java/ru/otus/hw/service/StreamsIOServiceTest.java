690c2992c344c56880d151947f741bad58f201d7
admin <a1exander.spb@yandex.ru>
2024-06-25 15:33:01 +0300

    1. Создание ветки hw02 2. Перенос материалов из hw01

---
+++ hw02/src/test/java/ru/otus/hw/service/StreamsIOServiceTest.java
@@ -0,0 +1,32 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StreamsIOServiceTest {
+
+    private PrintStream printStream;
+    private ArgumentCaptor<String> captor;
+    private IOService streamsIOService;
+
+    @BeforeEach
+    void setUp() {
+        printStream = Mockito.mock(PrintStream.class);
+        streamsIOService = new StreamsIOService(printStream);
+        captor = ArgumentCaptor.forClass(String.class);
+    }
+
+    @Test
+    void shouldPrintHelloWorld() {
+        String text = "Hello world";
+        streamsIOService.printLine(text);
+        Mockito.verify(printStream).println(captor.capture());
+        assertEquals(captor.getValue(), text);
+    }
+}
