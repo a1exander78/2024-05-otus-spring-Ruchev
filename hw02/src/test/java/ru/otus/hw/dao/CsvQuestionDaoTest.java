b098b1c335f3c8756208db5f9abe20edfb087e7b
admin <a1exander.spb@yandex.ru>
2024-06-26 13:55:03 +0300

    Добавлен тест ДАО

--- hw02/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw02/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -1,23 +1,29 @@
 package ru.otus.hw.dao;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.config.AppProperties;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Class CsvQuestionDao")
 class CsvQuestionDaoTest {
 
-    @Mock
-    private TestFileNameProvider fileNameProvider;
-
-    @InjectMocks
+    private AppProperties properties;
     private CsvQuestionDao csvQuestionDao;
 
-    @DisplayName("")
+    @BeforeEach
+    void setUp() {
+        properties = new AppProperties(3, "/questions.csv");
+        csvQuestionDao = new CsvQuestionDao(properties);
+    }
+
+    @DisplayName("should read question from file correctly")
     @Test
     void should() {
-
+        var question = csvQuestionDao.findAll().get(0);
+        var answers = question.answers();
+        assertThat(question.text()).isEqualTo("Which element of the periodic table is designated as \"Ag\"?");
     }
 }
