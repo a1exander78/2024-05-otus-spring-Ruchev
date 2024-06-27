dc617c25e1f5964a18e0ce86d6d21bdfa66ce127
admin <a1exander.spb@yandex.ru>
2024-06-27 13:32:32 +0300

    Добавлен класс конфигурации, откорректирован класс AppProperties

--- hw02/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw02/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -1,13 +1,10 @@
 package ru.otus.hw.config;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-@Configuration
-@PropertySource("classpath:application.properties")
 @Data
+@AllArgsConstructor
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
     // внедрить свойство из application.properties
@@ -15,12 +12,5 @@ public class AppProperties implements TestConfig, TestFileNameProvider {
 
     // внедрить свойство из application.properties
     private String testFileName;
-
-    public AppProperties(@Value("${test.rightAnswersCountToPass}") int rightAnswersCountToPass,
-                         @Value("${test.fileName}") String testFileName) {
-        this.rightAnswersCountToPass = rightAnswersCountToPass;
-        this.testFileName = testFileName;
-    }
-
 
 }
