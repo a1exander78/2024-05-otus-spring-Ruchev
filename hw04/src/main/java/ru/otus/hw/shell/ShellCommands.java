9f78515a1ccb60952b56a8ae6904c2edb2489043
admin <a1exander.spb@yandex.ru>
2024-07-09 16:24:10 +0300

    Исправления по замечанию

--- hw04/src/main/java/ru/otus/hw/shell/ShellCommands.java
+++ hw04/src/main/java/ru/otus/hw/shell/ShellCommands.java
@@ -3,14 +3,14 @@ package ru.otus.hw.shell;
 import lombok.RequiredArgsConstructor;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
-import ru.otus.hw.service.LocalizedIOServiceImpl;
+import ru.otus.hw.service.LocalizedIOService;
 import ru.otus.hw.service.TestRunnerServiceImpl;
 
 @RequiredArgsConstructor
 @ShellComponent
 public class ShellCommands {
 
-    private final LocalizedIOServiceImpl ioService;
+    private final LocalizedIOService ioService;
 
     private final TestRunnerServiceImpl testRunnerService;
 
