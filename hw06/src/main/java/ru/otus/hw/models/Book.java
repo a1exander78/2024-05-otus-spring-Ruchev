d75ff587a3177d0e07ad07bfa3e9e019ec0f0747
admin <a1exander.spb@yandex.ru>
2024-07-18 18:26:26 +0300

    Сущности автор и жанр перенесены на JPA

--- hw06/src/main/java/ru/otus/hw/models/Book.java
+++ hw06/src/main/java/ru/otus/hw/models/Book.java
@@ -1,5 +1,6 @@
 package ru.otus.hw.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,12 +8,20 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "book")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String title;
 
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "author_id")
     private Author author;
 
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "genre_id")
     private Genre genre;
 }
