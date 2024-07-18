2ee7e3541a0901360d75346217cbe192a2f6ed40
admin <a1exander.spb@yandex.ru>
2024-07-18 13:58:15 +0300

    Создание новой ветки для выполнения ДЗ №6. Перенос файлов из предыдущего

---
+++ hw06/src/test/resources/data.sql
@@ -0,0 +1,8 @@
+insert into authors(full_name)
+values ('Author1'), ('Author2'), ('Author3');
+
+insert into genres(name)
+values ('Genre1'), ('Genre2'), ('Genre3');
+
+insert into books(title, author_id, genre_id)
+values ('BookTitle1', 1, 1), ('BookTitle2', 2, 2), ('BookTitle3', 3, 3);
