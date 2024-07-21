2fee4249497a5f36c39fc0be22d75f5ed8b5f79f
admin <a1exander.spb@yandex.ru>
2024-07-21 14:29:40 +0300

    Поправлены файлы schema.sql и data.sql для соответствия с системой миграций

--- hw06/src/main/resources/data.sql
+++ hw06/src/main/resources/data.sql
@@ -1,8 +1,11 @@
-insert into authors(full_name)
+insert into author(full_name)
 values ('Author1'), ('Author2'), ('Author3');
 
-insert into genres(name)
+insert into genre(name)
 values ('Genre1'), ('Genre2'), ('Genre3');
 
-insert into books(title, author_id, genre_id)
+insert into book(title, author_id, genre_id)
 values ('BookTitle1', 1, 1), ('BookTitle2', 2, 2), ('BookTitle3', 3, 3);
+
+insert into comment(description, book_id)
+values ('Comment_1', 1), ('Comment_2', 2), ('Comment_3', 3);
--- hw06/src/main/resources/schema.sql
+++ hw06/src/main/resources/schema.sql
@@ -1,19 +1,26 @@
-create table authors (
+create table author (
     id bigserial,
     full_name varchar(255),
     primary key (id)
 );
 
-create table genres (
+create table genre (
     id bigserial,
     name varchar(255),
     primary key (id)
 );
 
-create table books (
+create table book (
     id bigserial,
     title varchar(255),
-    author_id bigint references authors (id) on delete cascade,
-    genre_id bigint references genres(id) on delete cascade,
+    author_id bigint references author(id) on delete cascade,
+    genre_id bigint references genre(id) on delete cascade,
+    primary key (id)
+);
+
+create table comment (
+    id bigserial,
+    description varchar(255),
+    book_id bigint references book(id) on delete cascade,
     primary key (id)
 );
