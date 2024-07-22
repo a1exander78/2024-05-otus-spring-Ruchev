0d01b9bba407bfb2ff88a1ad850fecbef0cd525d
admin <a1exander.spb@yandex.ru>
2024-07-22 16:24:05 +0300

    Изменения в наполнении тестовой БД (для наглядности).

--- hw06/src/test/resources/data.sql
+++ hw06/src/test/resources/data.sql
@@ -1,11 +1,11 @@
 insert into author(full_name)
-values ('Author1'), ('Author2'), ('Author3');
+values ('Author_Test_1'), ('Author_Test_2'), ('Author_Test_3');
 
 insert into genre(name)
-values ('Genre1'), ('Genre2'), ('Genre3');
+values ('Genre_Test_1'), ('Genre_Test_2'), ('Genre_Test_3');
 
 insert into book(title, author_id, genre_id)
-values ('BookTitle1', 1, 1), ('BookTitle2', 2, 2), ('BookTitle3', 3, 3);
+values ('Book_Test_1', 1, 1), ('Book_Test_2', 2, 2), ('Book_Test_3', 3, 3);
 
 insert into comment(description, book_id)
-values ('Comment_1', 1), ('Comment_2', 2), ('Comment_3', 3);
+values ('Comment_Test_1', 1), ('Comment_Test_2', 2), ('Comment_Test_3', 3), ('Comment_Test_4', 1);
